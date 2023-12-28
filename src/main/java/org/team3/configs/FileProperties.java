e4f03f7f12b6662862974524324a9edbe4dfabb6
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-29 01:38:33 +0900

    setting - 설정파일 파일경로

---
+++ src/main/java/org/team3/configs/FileProperties.java
@@ -0,0 +1,14 @@
+package org.team3.configs;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "file.upload")
+public class FileProperties {
+
+    private String path;
+    private String url;
+
+}
+
--- src/main/java/org/team3/configs/MvcConfig.java
+++ src/main/java/org/team3/configs/MvcConfig.java
@@ -1,13 +1,29 @@
 package org.team3.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableConfigurationProperties(FileProperties.class)
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private FileProperties fileProperties;
+
+    /**
+     * 정적 경로 설정
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(fileProperties.getUrl() + "**") // 현재 경로를 포함 하위 모든 파일
+                .addResourceLocations("file:///" + fileProperties.getPath());
+    }
 
     /**
      * 메세지 설정
