f08716f24ed5c656ae9f5fdb59e5eb19fce12d54
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-16 19:11:00 +0900

    mypage follow 구현중

--- src/main/java/org/team3/member/service/FollowService.java
+++ src/main/java/org/team3/member/service/FollowService.java
@@ -2,6 +2,8 @@ package org.team3.member.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.team3.member.entities.Member;
+import org.team3.member.repositories.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +13,11 @@ public class FollowService {
      * Follow 기능
      * 1월 16일 이지은
      */
+
+    private final MemberRepository memberRepository;
+
+    public void followUser(String followerUsername, String followingUsername) {
+
+    }
+
 }
