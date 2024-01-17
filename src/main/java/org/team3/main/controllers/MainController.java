18d31ec53fd154917a530fdb2cfc0b8056cf46aa
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-17 18:57:06 +0900

    로고누르면 홈화면 링크 수정

---
+++ src/main/java/org/team3/main/controllers/MainController.java
@@ -0,0 +1,26 @@
+package org.team3.main.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.team3.commons.ExceptionProcessor;
+import org.team3.commons.Utils;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController implements ExceptionProcessor {
+
+    private final Utils utils;
+
+    @ModelAttribute("addCss")
+    public String[] addCss() {
+        return new String[]{"main/style"};
+    }
+
+    @GetMapping("/")
+    public String index() {
+
+        return utils.tpl("main/index");
+    }
+}
