ea774e1e74b9305d9951406e815bb5c329c30a35
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-22 00:39:43 +0900

    팔로우 : 레포지토리 추가 - MemberSearch 내용 공통기능을 뻄 -> commons/RequestPaging

--- src/main/java/org/team3/member/controllers/MemberSearch.java
+++ src/main/java/org/team3/member/controllers/MemberSearch.java
@@ -1,13 +1,17 @@
 package org.team3.member.controllers;
 
 import lombok.Data;
+import org.team3.commons.RequestPaging;
 
 /**
  * 검색 데이터 -> 요청 데이터!
  * 검색 데이터를 담을 커맨드 객체가 필요함
+ *
+ * 공통기능을 뻄 -> commons/RequestPaging
+ * protected int page = 1;
+ * protected int limit = 20;
  */
 @Data
-public class MemberSearch {
-    protected int page = 1;
-    protected int limit = 20;
+public class MemberSearch extends RequestPaging {
+
 }
