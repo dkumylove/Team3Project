e38293859da71183696a035cf24dd336dc7cdb18
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-30 19:53:38 +0900

    -

--- src/main/java/org/team3/upbit/controllers/UpBitController.java
+++ src/main/java/org/team3/upbit/controllers/UpBitController.java
@@ -1,28 +1,37 @@
-//package org.team3.upbit.controllers;
+package org.team3.upbit.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.team3.commons.Utils;
+import org.team3.upbit.entities.UpBitTicker;
+import org.team3.upbit.service.UpBitService;
+import org.team3.upbit.service.UpBitTickerSearch;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/upbit")
+@RequiredArgsConstructor
+public class UpBitController {
+
+    private final Utils utils;
+    private final UpBitService upBitService;
+
+    @GetMapping("/main")
+    public String index(@ModelAttribute UpBitTickerSearch search, Model model) {
+//        List<String> addCommonScript = new ArrayList<>();
+//        addCommonScript.add("upbit");
 //
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.team3.commons.Utils;
+//        model.addAttribute("addCommonScript",addCommonScript);
+//        List<UpBitTicker> date = upBitService.getList(search);
 //
-//@Controller
-//@RequestMapping("/upbit")
-//@RequiredArgsConstructor
-//public class UpBitController {
-//
-//    private final Utils utils;
-//
-//    @GetMapping("/main")
-//    public String index(Model model) {
-////        List<String> addCommonScript = new ArrayList<>();
-////        addCommonScript.add("upbit");
-////
-////        model.addAttribute("addCommonScript",addCommonScript);
-//
-//        model.addAttribute("addCommonScript", new String[] {"upbit"});
-//
-//        return utils.tpl("upbit/main");
-//    }
-//}
+//        model.addAttribute("items", date);
+        model.addAttribute("addCommonScript", new String[] {"upbit2"});
+
+        return utils.tpl("upbit/main");
+    }
+}
