5d0708554b8a959ad9857ed134b4cb11e0ac7cf2
dani <bin0696@naver.com>
2024-02-01 17:53:38 +0900

    지표 상세 페이지 구현 중

--- src/main/java/org/team3/rank/RankController.java
+++ src/main/java/org/team3/rank/RankController.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.team3.admin.option.Repository.OptionRankRepository;
 import org.team3.admin.option.entities.OptionRank;
 import org.team3.admin.option.service.OptionRankInfoService;
+import org.team3.board.controllers.BoardDataSearch;
+import org.team3.board.entities.BoardData;
+import org.team3.commons.ListData;
 import org.team3.commons.Utils;
 import org.team3.member.service.MemberInfoService;
 
@@ -30,5 +35,18 @@ public class RankController {
         model.addAttribute("member", member);
         model.addAttribute("optionList", optionList);
         return utils.tpl("rank/index");
+    }
+
+
+
+    @GetMapping("/{optionname}")
+    public String list(@PathVariable("optionname") String optionname,
+                       @ModelAttribute OptionRank OptionRank, Model model) {
+
+        OptionRank data = optionRankInfoService.getOptionBoard(optionname);
+
+        model.addAttribute("optionrank", data);
+
+        return utils.tpl("rank/listpage");
     }
 }
